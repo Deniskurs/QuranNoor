6dc4a0a89f39c96ee52d3bbc6e41d2fa46fe888e
Deniskurs <deniskursevicius@gmail.com>
2025-10-31 12:21:48 +0000

    Initial Commit

---
+++ QuranNoor/QuranNoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranNoorApp.swift
+//  QuranNoor
+//
+//  Created by Denis Kursevicius on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuranNoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
